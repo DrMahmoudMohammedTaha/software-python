5c3aec1189e8156c22f134e6b9200852d8757539
Mahmoud Taha <mahmoud.taha17@feng.bu.edu.eg>
2024-05-25 08:53:53 +0300

    Update python_sheet.py

--- python_sheet.py
+++ python_sheet.py
@@ -192,7 +192,7 @@ adults = filter(myFunc, ages)
 # At first step, first two elements of sequence are picked and the result is obtained.
 # Next step is to apply the same function to the previously attained result 
 # and the number just succeeding the second element and the result is again stored.
-
+# A third argument can be passed to indicate the starting value
 # importing functools for reduce() 
 import functools 
   
